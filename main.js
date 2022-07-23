c599d3ed0c394eea08d4eacb46ef6ec57d405c69
ter-gav <86326060+ter-gav@users.noreply.github.com>
2022-07-23 17:06:41 +0200

    added conditional rendering

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      inventory: 9,
+      onSale: true,
+    };
+  },
+});
